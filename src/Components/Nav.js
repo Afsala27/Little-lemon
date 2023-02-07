218734ecd4c470a13cbe27b5b257ea65398b8aa9
Afsala27 <afsalabbasna@gmail.com>
2023-02-07 13:12:44 +0530

    Simplify the boilerplate app 2

---
+++ src/Components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <>
+    <nav>
+        <img src='Logo.png' alt='Logo'/>
+        <ul>
+            <li> <a href='/home'>Home</a> </li>
+            <li> <a href='/about'>About</a> </li>
+            <li> <a href='/menu'>Menu</a> </li>
+            <li> <a href='/reservation'>Reservation</a> </li>
+            <li> <a href='/orderonline'>Order Online</a> </li>
+            <li> <a href='/login'>Login</a> </li>
+        </ul>
+    </nav>
+    </>
+  )
+}
